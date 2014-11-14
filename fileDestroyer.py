99d07cedac607b902362df04db38ae9a5c755e9d
Tommy Lin <tommylin1998@outlook.com>
2014-11-14 09:12:50 -0500

    Finish the basic program

--- fileDestroyer.py
+++ fileDestroyer.py
@@ -1,2 +1,19 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
+fileName = input('What file should I destroy? ')
+times = int(input('How many times should I overwrite it? '))
+
+f = open(fileName, "r+")
+fileContents = f.read()
+letters = len(fileContents)
+
+print('WARNING: This will destroy all data in {}'.format(fileName))
+response = input('Are you sure you want to continue? ')
+
+if response.lower() == 'yes':
+    for i in range(times):
+        f.seek(0)
+        print('Pass {}: Writing file with all {}\'s'.format(i + 1, chr(65 + i)))
+        f.write('{}'.format(chr(65 + i) * letters))
+
+f.close()
